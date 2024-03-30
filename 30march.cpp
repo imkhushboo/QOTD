8b508b4058420a223b5b31d3070769e9830ce7cf
imkhushboo <vkhushboo540@gmail.com>
2024-03-30 17:25:04 +0530

    Count Subarrays Where Max Element Appears at Least K Times

---
+++ 30march.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int helper(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        int i = 0, j = 0;
+        int n = nums.size();
+        int cnt = 0;
+
+        while (j < n)
+        {
+            mp[nums[j]]++;
+            while (mp.size() > k)
+            {
+                mp[nums[i]]--;
+                if (mp[nums[i]] == 0)
+                {
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+            cnt += (j - i + 1);
+            j++;
+        }
+
+        return cnt;
+    }
+
+    int subarraysWithKDistinct(vector<int> &nums, int k)
+    {
+        return helper(nums, k) - helper(nums, k - 1);
+    }
+};
