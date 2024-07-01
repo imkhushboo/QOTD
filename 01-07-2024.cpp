7ad6441b494f116bdb34dcedde12987b416e7d9e
Khushboo <66317974+imkhushboo@users.noreply.github.com>
2024-07-01 22:52:06 +0530

    01-07-2024.cpp

---
+++ 01-07-2024.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+       int n = arr.size(); 
+    for(int i=2;i<n;i++)
+    {
+        if(arr[i]%2 and arr[i-1]%2 and arr[i-2]%2) return true;
+    }
+        return false;
+    }
+};
