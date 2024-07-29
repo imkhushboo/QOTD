5644c16a1369c6473828fc3a473c61d3a54a8eb8
Khushboo <66317974+imkhushboo@users.noreply.github.com>
2024-07-29 23:11:48 +0530

    29-07-2024.cpp

---
+++ 29-07-2024.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n=rating.size();
+        int cnt=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if(rating[i]<rating[j] && rating[j]<rating[k]){
+                        cnt++;
+                    }
+                    else if(rating[i]>rating[j] && rating[j]>rating[k]) cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
