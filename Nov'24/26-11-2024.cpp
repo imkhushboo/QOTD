f4e8e3698590c2a2c665248797a3c992d3cb69ed
Khushboo <66317974+imkhushboo@users.noreply.github.com>
2024-11-26 23:02:33 +0530

    26-11-2024.cpp

---
+++ Nov'24/26-11-2024.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int>indegree(n,0);
+        
+        for(auto x:edges)
+        {
+            indegree[x[1]]++;
+        }
+        int edge = -1;
+        int cnt = 0;
+        for(int i=0 ;i<n;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                cnt++;
+                edge = i;
+            }
+        }
+        
+        if(cnt > 1) return -1;
+        return edge;
+        
+    }
+};
