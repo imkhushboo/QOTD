5125c4260647319f8934fb8d30c8a2b775a92bda
Khushboo <66317974+imkhushboo@users.noreply.github.com>
2025-01-06 23:00:41 +0530

    06-01-2024.cpp

---
+++ Jan'25/06-01-2024.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        vector<int>res(n,0);
+
+        for(int i=0;i<n;i++){
+            int ans = 0;
+            for(int j=0;j<n;j++){
+                if(boxes[j] == '1'){
+                    ans += abs(j-i);
+                }
+
+
+            }
+            res[i] = ans;
+        }
+
+        return res;
+
+        
+    }
+};
